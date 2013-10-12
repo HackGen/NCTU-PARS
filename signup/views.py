dcb26c15c00ac693e26f4a596dfabbc77922d43f
dv <wdv4758h@gmail.com>
2013-10-12 14:13:31 +0800

    modify add some field to RegSysForm modify fields' widget.attrs in signup view.py

--- signup/views.py
+++ signup/views.py
@@ -1,8 +1,11 @@
+# -*- coding:utf-8 -*-
+
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from signup.models import RegSysForm
+from django import forms
 
 def signup(request):
     if request.method == 'POST':
@@ -13,11 +16,17 @@ def signup(request):
     else:
         form = RegSysForm()
 
-    form.fields['username'].widget.attrs['class'] = 'form-control'
-    form.fields['password1'].widget.attrs['class'] = 'form-control'
-    form.fields['password2'].widget.attrs['class'] = 'form-control'
-    form.fields['email'].widget.attrs['class'] = 'form-control'
-    form.fields['last_name'].widget.attrs['class'] = 'form-control'
-    form.fields['first_name'].widget.attrs['class'] = 'form-control'
+    for field in form.fields.keys():
+        field_obj = form.fields[field]
+
+        field_obj.widget.attrs = {
+                'class' : 'form-control',
+                'placeholder' : field_obj.help_text}
+        field_obj.help_text = ''
+
+    ##hide password field
+    ##lopking for better solution
+    #form.fields['password1'].widget = forms.HiddenInput()
+    #form.fields['password2'].widget = forms.HiddenInput()
 
     return render_to_response('signup.html', {'form': form}, context_instance=RequestContext(request))
